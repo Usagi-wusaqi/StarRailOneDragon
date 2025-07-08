5eb69e8c418567ee919e180aa133df64f3d0e410
DoctorReid <doctorreid2024@outlook.com>
2025-07-08 22:13:17 +0800

    feat: 锄大地增加一个重置记录按钮 #487

--- src/sr_od/gui/interface/world_patrol/world_patrol_run_interface.py
+++ src/sr_od/gui/interface/world_patrol/world_patrol_run_interface.py
@@ -1,10 +1,13 @@
-from PySide6.QtWidgets import QWidget
-from qfluentwidgets import HyperlinkCard, FluentIcon
 from typing import Optional
 
+from PySide6.QtWidgets import QWidget
+from qfluentwidgets import HyperlinkCard, FluentIcon
+
 from one_dragon.base.operation.application_base import Application
-from one_dragon_qt.widgets.column import Column
+from one_dragon.utils.log_utils import log
 from one_dragon_qt.view.app_run_interface import AppRunInterface
+from one_dragon_qt.widgets.column import Column
+from one_dragon_qt.widgets.setting_card.push_setting_card import PushSettingCard
 from sr_od.app.sr_application import SrApplication
 from sr_od.app.world_patrol.world_patrol_app import WorldPatrolApp
 from sr_od.context.sr_context import SrContext
@@ -33,7 +36,15 @@ class WorldPatrolRunInterface(AppRunInterface):
                                       url='https://onedragon-anything.github.io/sr/zh/docs/feat_world_patrol.html')
         content.add_widget(self.help_opt)
 
+        self.reset_opt = PushSettingCard(icon=FluentIcon.SYNC, title='重置运行记录', text='重置')
+        self.reset_opt.clicked.connect(self.on_reset_clicked)
+        content.add_widget(self.reset_opt)
+
         return content
 
     def get_app(self) -> Application:
         return WorldPatrolApp(self.ctx)
+
+    def on_reset_clicked(self) -> None:
+        self.ctx.world_patrol_record.reset_record()
+        log.info('运行记录已重置')
