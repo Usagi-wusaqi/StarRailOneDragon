f7ad19cacef45c4a98219a8860277b5dc39dbc79
MLGShimadaYasuo <134863853+MLGShimadaYasuo@users.noreply.github.com>
2025-07-09 02:46:17 +0100

    更新 领取记忆残晶 周期 (#496)

--- src/sr_od/app/memory_crystal_shard/memory_crystal_shard_run_record.py
+++ src/sr_od/app/memory_crystal_shard/memory_crystal_shard_run_record.py
@@ -1,10 +1,11 @@
 from typing import Optional
 
-from one_dragon.base.operation.application_run_record import AppRunRecord
+from one_dragon.base.operation.application_run_record import AppRunRecord, AppRunRecordPeriod
 
 
 class MemoryCrystalShardRunRecord(AppRunRecord):
 
     def __init__(self, instance_idx: Optional[int] = None, game_refresh_hour_offset: int = 0):
         AppRunRecord.__init__(self, 'memory_crystal_shard', instance_idx=instance_idx,
+                              record_period=AppRunRecordPeriod.WEEKLY,
                               game_refresh_hour_offset=game_refresh_hour_offset)
